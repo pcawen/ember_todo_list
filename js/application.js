2f9f5624754fd6d3fe5ed06d359bceaf219b0dd2
pcawen <pablo.cawen@gmail.com>
2014-06-10 09:05:30 -0300

    enable RESTAdapter

--- js/application.js
+++ js/application.js
@@ -3,7 +3,21 @@ window.Todos = Ember.Application.create({
 	LOG_TRANSITIONS: true
 });
 
+Todos.Store = DS.Store.extend({
+  revision: 11,
+  //url: "http://www.example.com"
+});
+
 //Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
 Todos.ApplicationAdapter = DS.RESTAdapter.reopen({
-	url: 'http://localhost:3000'
+	host: 'http://localhost:3000'
 });
+
+/*App.ApplicationAdapter = DS.RESTAdapter.extend({
+  host: 'https://otherhost.com',
+  ajax: function(url, method, hash) {
+    hash.crossDomain = true;
+    hash.xhrFields = {withCredentials: true};
+    return this._super(url, method, hash);
+  }
+});*/
